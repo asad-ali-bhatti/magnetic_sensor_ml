aae7ac4dad93ff959c7563aa93d8b01db6d6b731
Asad Ali Bhatti <asad.ali.bhatti@ucm.agency>
2019-10-06 21:51:09 +0200

    final version

--- test_model.py
+++ test_model.py
@@ -3,7 +3,8 @@ from tensorflow import keras
 import pandas
 import psycopg2
 from sklearn.model_selection import train_test_split
-import ipdb
+import matplotlib.pyplot as plt
+
 
 import numpy as np
 
@@ -20,6 +21,7 @@ query = '''
   SELECT device_name, reading, device_id, row FROM(
     SELECT d.name as device_name, r.magnitude as reading, d.id as device_id, row_number() OVER (PARTITION BY d.id ) as row 
     FROM readings r INNER JOIN devices d ON r.device_id = d.id
+    WHERE r.reading_type != 'predict'
     ORDER BY r.created_at DESC
   ) as readings_per_device 
  '''
@@ -33,53 +35,60 @@ devices_data_frame = pandas.DataFrame(pd_devices, columns=['name', 'id'])
 no_of_devices = devices_data_frame['name'].size
 print(pd_query)
 
-devices = train['device_id']-1
-readings = train['reading']
+train_devices = train['device_id']-1
+train_readings = train['reading']
+test_devices = test['device_id']-1
+test_readings = test['reading']
 # print(make_sublist_of_data(data_frame['device_name']))
 print(train['reading'].shape)
 print(train['device_id'].shape)
 model = keras.Sequential([
-    keras.layers.Dense(100, input_shape=(1,)),
-    keras.layers.Dense(50, activation='relu'),
-    keras.layers.Dense(no_of_devices, name= 'DenseOut',activation='softmax'),
-    keras.layers.Dense(50, activation='relu'),
-    keras.layers.Dense(50, activation='relu'),
-    keras.layers.Dense(no_of_devices, activation='softmax'),
+    keras.layers.Dense(500, input_shape=(1,)),
+    keras.layers.Dense(50, name='hiddenLayer-1', activation='relu'),
+    keras.layers.Dense(50, name='hiddenLayer-2', activation='relu'),
+    keras.layers.Dense(50, name='hiddenLayer-3', activation='relu'),
+    keras.layers.Dense(no_of_devices, name='outputLayer', activation='softmax'),
 ])
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-model.fit(readings, devices, epochs=7)
-
-test_query = '''
-  SELECT device_name, reading, device_id, row FROM(
-    SELECT d.name as device_name, r.magnitude as reading, d.id as device_id, row_number() OVER (PARTITION BY d.id ) as row
-    FROM readings r INNER JOIN devices d ON r.device_id = d.id
-    where r.reading_type = 'test'
-    ORDER BY r.created_at DESC
-  ) as readings_per_device
- '''
-pd_query = pandas.read_sql_query(test_query, db_client)
-data_frame = pandas.DataFrame(pd_query, columns=['device_name', 'reading', 'device_id'])
-devices = test['device_id']-1
-readings = test['reading']
-loss, acc = model.evaluate(readings, devices)
-print('Accuracy: ', acc)
+history = model.fit(train_readings, train_devices, validation_data=[test_readings, test_devices], epochs=10, batch_size=16, verbose=1)
+keras.utils.plot_model(model, show_shapes=True, to_file='graph.png')
+print(model.summary())
+# =============TRAINING HISTORY==============
+# Plot training & validation accuracy values
+# plt.plot(history.history['acc'])
+# plt.plot(history.history['val_acc'])
+# plt.plot(history.history['loss'])
+# plt.plot(history.history['val_loss'])
 #
-predict_query = '''
-    SELECT d.name as device_name, r.magnitude as reading, d.id as device_id 
-    FROM readings r 
-    INNER JOIN devices d ON r.device_id = d.id
-    WHERE r.reading_type = 'predict'
-    ORDER BY r.created_at DESC 
-    LIMIT 100
- '''
-
-pd_query = pandas.read_sql_query(predict_query, db_client)
-data_frame = pandas.DataFrame(pd_query, columns=['device_name', 'reading', 'device_id'])
-devices = data_frame['device_id']-1
-readings = data_frame['reading']
-prediction = model.predict(readings)
+# plt.title('Model accuracy')
+# plt.ylabel('Accuracy/Loss')
+# plt.xlabel('Epoch')
+# plt.legend(['Training Accuracy', 'Validation Accuracy', 'Loss', 'Validation Loss'], loc='upper right')
+# plt.show()
+#
+# input("Press Enter to continue...")
+# =============END TRAINING HISTORY==============
+# devices = test['device_id']-1
+# readings = test['reading']
+# loss, acc = model.evaluate(readings, devices)
+# print('Accuracy: ', acc)
+# #
+# predict_query = '''
+#     SELECT d.name as device_name, r.magnitude as reading, d.id as device_id
+#     FROM readings r
+#     INNER JOIN devices d ON r.device_id = d.id
+#     WHERE r.reading_type = 'predict'
+#     ORDER BY r.created_at DESC
+#     LIMIT 100
+#  '''
+#
+# pd_query = pandas.read_sql_query(predict_query, db_client)
+# data_frame = pandas.DataFrame(pd_query, columns=['device_name', 'reading', 'device_id'])
+# devices = data_frame['device_id']-1
+# readings = data_frame['reading']
+# prediction = model.predict(readings)
 # print('prediction: ', prediction)
-predicted_device = devices_data_frame['name'][np.argmax(prediction[0])]
-print(devices_data_frame)
-print('Predicted Device: ', predicted_device)
+# predicted_device = devices_data_frame['name'][np.argmax(prediction[0])]
+# print(devices_data_frame)
+# print('Predicted Device: ', predicted_device)
